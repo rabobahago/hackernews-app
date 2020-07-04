b6d5ad6b9c32858245d72aab9000e717ab9ce98e
rabobahago <rabobahago@gmail.com>
2020-07-04 10:21:27 +0100

    http request made used axios

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
-import React from 'react';
-const App = ()=><h1>Hello World!</h1>
+import React, {useEffect, useState} from 'react';
+import {getStoryIds} from './services/hnApi'
+const App = ()=>{
+    const [storyIds, setStoryIds] = useState([]);
+    useEffect(()=>{
+        getStoryIds().then(data =>setStoryIds(data));
+    }, [])
+    return(
+        <h1>{JSON.stringify(storyIds)}</h1>
+    )
+}
 export default App;
