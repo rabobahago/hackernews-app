1ec1021a44b5c950f8c03d7693efe087d8277067
rabobahago <rabobahago@gmail.com>
2020-07-05 09:55:59 +0100

    getting the real hackernews from api call

---
+++ src/components/Story.js
@@ -0,0 +1,16 @@
+import React, {useState, useEffect} from 'react';
+import {getStory} from '../services/hnApi'
+
+export const Story = ({storyId})=>{
+    const [story, setStory] = useState({})
+ useEffect(()=>{
+     getStory(storyId).then(data => data && data.url && setStory(data))
+ }, [])
+ return story && story.url ? (
+     <>
+     <a href={story.url}><p>{story.title}</p></a>
+     By: <p>{story.by}</p>
+     posted: <p>{story.time}</p>
+     </>
+ ) : null;
+}
--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import {getStoryIds, getStory} from '../services/hnApi'
-
+import {Story} from '../components/Story'
 export const StoriesContainer = ()=>{
     const [storyIds, setStoryIds] = useState([]);
     useEffect(()=>{
         getStoryIds().then(data =>setStoryIds(data));
     }, [])
-    return(
-        <h1>{JSON.stringify(storyIds)}</h1>
-    )
+    return storyIds.map(storyId =>(
+        <Story storyId={storyId}/>
+    ))
 }
